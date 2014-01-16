1a8c8ad3006df9e0daa75243dd6a157df493759e
jcbotha <jcbotha@me.com>
2014-01-16 08:37:01 -0800

    Updated Files

---
+++ Elevator/src/elevator/elevate.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package elevator;
+
+/**
+ *
+ * @author chrisbotha
+ */
+public class elevate {
+    
+    
+    /**
+Elevator Project
+
+- elevCurrentFloor	:int
+- elevCurrentPath	:int
+- pushBtnFloor		:int
+- extPush			:int
+- buttonNumber		:int
+- buttonStop		:bool
+- buttonEmerg		:bool
+
+-----------
+
++extButtonPush()	:bool
++intButtonPush()	:int
+
++getBtnPushFloor()	:int
++getElevFloor()		:int
++getElevPath()		:int
++setElevPath() 		:void
+
+*/
+
+
+	public static void main( String[] args ){
+
+	}
+	
+	public static void extButtonPush(){
+	}
+	
+	public static int intButtonPush() {
+	}
+	
+	public static int getBtnPushFloor() {
+	}
+	
+	public static int getElevFloor() {
+	}
+	
+	public static int getElevPath() {
+	}
+	
+	public static int setElevPath() {
+	}
+	
+	
+}
+
